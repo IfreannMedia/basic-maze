a935d7d3c644af0f454528c6be428f7f51229180
Wario <37343942+IfreannMedia@users.noreply.github.com>
2022-02-05 16:12:47 +0100

    created recursive script
    for the recursive backtraging maze generation algorithm

---
+++ Assets/scripts/Recursive.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recursive : Maze
+{
+
+    public override void SetEmptyCoordinates()
+    {
+    }
+}
